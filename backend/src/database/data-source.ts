d64bd1208a29fb95f41b352dbb2e407708439318
khlkarim <karimkahili002@gmail.com>
2025-11-11 23:59:55 +0100

    dockerized the app

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config(); // Loads .env if it exists
 
 export const AppDataSource = new DataSource({
   type: process.env.DATABASE_TYPE,
