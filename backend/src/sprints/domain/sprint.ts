99087b93ff0c0092ac46c2a30c6075586f7daee5
khlkarim <karimkahili002@gmail.com>
2025-11-12 23:38:43 +0100

    defined the core entities in the backend

---
+++ backend/src/sprints/domain/sprint.ts
@@ -0,0 +1,45 @@
+import { User } from '../../users/domain/user';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Sprint {
+  @ApiProperty({
+    type: () => User,
+    nullable: false,
+  })
+  createdBy: User;
+
+  @ApiProperty({
+    type: () => Date,
+    nullable: false,
+  })
+  endDate: Date;
+
+  @ApiProperty({
+    type: () => Date,
+    nullable: false,
+  })
+  startDate: Date;
+
+  @ApiProperty({
+    type: () => String,
+    nullable: true,
+  })
+  goal?: string | null;
+
+  @ApiProperty({
+    type: () => String,
+    nullable: false,
+  })
+  name: string;
+
+  @ApiProperty({
+    type: String,
+  })
+  id: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
---
+++ backend/src/tasks/domain/task.ts
@@ -0,0 +1,52 @@
+import { User } from '../../users/domain/user';
+import { Sprint } from '../../sprints/domain/sprint';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Task {
+  @ApiProperty({
+    type: () => User,
+    nullable: false,
+  })
+  reporter: User;
+
+  @ApiProperty({
+    type: () => User,
+    nullable: false,
+  })
+  assignee: User;
+
+  @ApiProperty({
+    type: () => Sprint,
+    nullable: false,
+  })
+  sprint: Sprint;
+
+  @ApiProperty({
+    type: () => Date,
+    nullable: false,
+  })
+  dueDate: Date;
+
+  @ApiProperty({
+    type: () => String,
+    nullable: true,
+  })
+  description?: string | null;
+
+  @ApiProperty({
+    type: () => String,
+    nullable: false,
+  })
+  title: string;
+
+  @ApiProperty({
+    type: String,
+  })
+  id: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
